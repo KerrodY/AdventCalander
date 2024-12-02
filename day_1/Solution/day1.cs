644a67720b3b674ecf6d6032634bed396414255b
Isaiah Weidl <2137144+IWeidl@users.noreply.github.com>
2024-12-02 22:26:16 +1100

    Add day 1 C# solution

---
+++ day_1/Solution/day1.cs
@@ -0,0 +1,19 @@
+﻿var fileContents = File.ReadAllLines("Inputs/day1.txt").ToList();
+var list1 = new List<int>();
+var list2 = new List<int>();
+var totalDistance = 0;
+
+foreach (var line in fileContents)
+{
+    var splitLine = line.Split("   ").ToList();
+    list1.Add(int.Parse(splitLine[0]));
+    list2.Add(int.Parse(splitLine[1]));
+}
+
+list1.Sort();
+list2.Sort();
+
+foreach (var (index, item) in list1.Index())
+    totalDistance += Math.Abs(item - list2[index]);
+
+Console.WriteLine(totalDistance);
